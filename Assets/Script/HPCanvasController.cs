780eb2216cc448f4a80482e9fac427bba805333d
soccer4kzm8 <soccer4.kzm8@gmail.com>
2023-07-01 18:15:03 +0900

    canvasの位置調整

--- Assets/Script/HPCanvasController.cs
+++ Assets/Script/HPCanvasController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class HPCanvasController : MonoBehaviour
 {
+    [SerializeField] Transform _targetTransform;
     private void Update()
     {
         this.transform.rotation = Camera.main.transform.rotation;
+        this.transform.position = new Vector3(_targetTransform.position.x, _targetTransform.position.y + 1f, _targetTransform.position.z +1f);
     }
 }
